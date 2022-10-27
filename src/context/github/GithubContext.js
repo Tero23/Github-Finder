dc8d57b281daeae277f57108f166d30dfc40efa5
Tro Doumanian <doumaniant@gmail.com>
2022-10-27 18:05:22 +0300

    Initial Commit

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,71 @@
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
+
+const GithubContext = createContext();
+
+const GITHUB_URL = "https://api.github.com";
+
+export const GithubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    user: {},
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer(githubReducer, initialState);
+
+  const searchUsers = async (text) => {
+    setLoading();
+    const params = new URLSearchParams({
+      q: text,
+    });
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`);
+
+    const { items } = await response.json();
+
+    dispatch({
+      type: "GET_USERS",
+      payload: items,
+    });
+  };
+
+  // Get Single User
+  const getUser = async (login) => {
+    setLoading();
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`);
+
+    if (response.status === 404) {
+      window.location = "/notfound";
+    } else {
+      const data = await response.json();
+
+      dispatch({
+        type: "GET_USER",
+        payload: data,
+      });
+    }
+  };
+
+  // Set loading
+  const setLoading = () => dispatch({ type: "SET_LOADING" });
+
+  const clearUsers = () => dispatch({ type: "CLEAR_USERS" });
+
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        loading: state.loading,
+        user: state.user,
+        searchUsers,
+        clearUsers,
+        getUser,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubContext;
